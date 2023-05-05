f90e02af4074a5c55971c2160e071fcda309c91b
konda1212 <127831510+konda1212@users.noreply.github.com>
2023-05-05 10:52:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,8 @@ region  = "ap-south-1"
 access_key = "AKIAYMIVTNW7F4IBV3P2"
 secret_key = "5A2NMyqD25T+7djSKfOPgETcxe5BGqqPni4QABa0"
 }
-
-resource "aws_s3_bucket" "prasad" {
-bucket = "prasad70950"
+resource "aws_s3_bucket" "example" {
+bucket = "prasad7095015"
 tags   = {
 Name   = "dev"
  }
